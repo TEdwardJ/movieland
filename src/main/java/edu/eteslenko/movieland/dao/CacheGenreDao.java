f2799d0b8b954efa90839a2ad5594ac48e2415fa
JavaTed <30757832+JavaTed@users.noreply.github.com>
2018-11-22 21:53:05 +0200

    t-1 Fixing getting cache collection directly

--- src/main/java/edu/eteslenko/movieland/dao/CacheGenreDao.java
+++ src/main/java/edu/eteslenko/movieland/dao/CacheGenreDao.java
@@ -4,13 +4,12 @@ import edu.eteslenko.movieland.entity.Genre;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Repository
@@ -19,7 +18,6 @@ public class CacheGenreDao implements GenreDao {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
     private final ReentrantReadWriteLock refreshLock = new ReentrantReadWriteLock();
-    private CountDownLatch firstRefreshLatch = new CountDownLatch(1);
     private GenreDao genreDao;
     private List<Genre> genreList;
 
@@ -31,38 +29,31 @@ public class CacheGenreDao implements GenreDao {
     @Override
     public List<Genre> getAll() {
         List<Genre> localGenreList;
-        if (firstRefreshLatch.getCount() > 0) {
-            logger.debug("Waiting for first refresh started");
-            try {
-                firstRefreshLatch.await();
-            } catch (InterruptedException e) {
-                logger.error("Interruption error when waiting for first refresh completed {}", e);
-            }
-            logger.debug("Waiting for first refresh completed");
-        }
-        Lock readLock = refreshLock.readLock();
+
+        ReentrantReadWriteLock.ReadLock readLock = refreshLock.readLock();
         logger.debug("Going to acquire read lock");
         readLock.lock();
-        localGenreList = genreList;
-        readLock.unlock();
-        logger.debug("Read unlock");
-        return localGenreList;
+        try {
+            localGenreList = new ArrayList(genreList);
+            return localGenreList;
+        }finally{
+            readLock.unlock();
+            logger.debug("Read unlock");
+        }
     }
 
     @Scheduled(fixedRateString = "${app.cacheRefreshPeriod}")
     protected void refresh() {
-        List<Genre> genreList = genreDao.getAll();
-
-        Lock writeLock = refreshLock.writeLock();
+        ReentrantReadWriteLock.WriteLock writeLock = refreshLock.writeLock();
 
         logger.debug("Going to acquire write lock");
         writeLock.lock();
-        this.genreList = genreList;
-        writeLock.unlock();
-        logger.debug("Refresh is completed and write unlock is done");
-
-        if (firstRefreshLatch.getCount() > 0) {
-            firstRefreshLatch.countDown();
+        try {
+            List<Genre> genreList = new ArrayList(genreDao.getAll());
+            this.genreList = genreList;
+        } finally {
+            writeLock.unlock();
+            logger.debug("Refresh is completed and write unlock is done");
         }
     }
 }
